7b3842d56a206ab4144613c94b43789eb8c33fd8
Michael Fashola <michaelfashola@gmail.com>
2016-05-13 16:15:39 -0400

    assets setup and management

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require angular/angular
+//= require d3/d3
+// require turbolinks
 //= require_tree .
